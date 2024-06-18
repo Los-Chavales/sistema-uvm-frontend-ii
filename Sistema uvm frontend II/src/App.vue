4b80fc387534526cffff386aca7525e1eb778c1a
Crisangelly <hernandezfernandezcd@uvm.edu.ve>
2024-06-18 16:52:09 -0400

    se agregó footer

--- Sistema uvm frontend II/src/App.vue
+++ Sistema uvm frontend II/src/App.vue
@@ -1,12 +1,24 @@
 <script setup>
 /* import { RouterLink, RouterView } from 'vue-router' */
 import Header from "./common/Header.vue"
+import Footer from "./common/Footer.vue"
 </script>
 
 <template>
   <Header />
+  <div class="contenido">
+
+  </div>
+  <Footer />
 </template>
 
 <style lang="scss" scoped>
 
+ .contenido{
+  min-height: 100vh;
+ }
+
+ body{
+  height: 100vh;
+}
 </style>
