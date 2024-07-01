eb54225f540b2a67c7672e4b36ebd945ca5ce807
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-06-30 23:07:51 -0400

    GDP-14-botón periodos

---
+++ frontend/src/components/buttons/Submit_Button.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    message: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <button class="submitB" type="submit">{{ message }}</button>
+</template>
+
+<style style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+.submitB {
+    padding: 6px 6px;
+    flex-shrink: 0;
+    border: 0;
+    border-radius: 10px;
+    color: $color7;
+    font-family: "poppins";
+    font-size: 32px;
+    font-weight: 600;
+    background-color: $color4;
+
+}
+
+.submitB:hover {
+    cursor: pointer;
+}
+</style>
