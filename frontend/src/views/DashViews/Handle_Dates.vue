6e16478212315271dc950b991352d22b8a3cbaa2
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-07-30 10:07:38 -0400

    GDP-14 consultar fechas-tabla store

--- frontend/src/views/DashViews/Handle_Dates.vue
+++ frontend/src/views/DashViews/Handle_Dates.vue
@@ -3,40 +3,68 @@ import DashTable from '@/components/DashMain/DashTable.vue';
 import { computed, onMounted, ref, watch } from 'vue';
 import { useSchedulesStore } from '@/stores/Dash_Stores/schedules';
 import { buttonStateStore } from '@/stores/buttonState';
+import { useEventsStore } from '@/stores/events';
 import Modal_assigned_schedules from '@/components/modals/Modal_assigned_schedules.vue';
 import CreateSchedule from '@/components/DashMain/Create-Schedule.vue';
 import EditSchedule from '@/components/DashMain/Edit-Schedule.vue';
+import Entity_Button from '@/components/buttons/Entity_Button.vue';
+import { usePeriodsStore } from '@/stores/periods';
 
 const buttonState = buttonStateStore()
 const buttonChange = buttonState.changeState
 
 let schedulesStore = useSchedulesStore();
+const dateStore = useEventsStore();
+const periodStore = usePeriodsStore()
 
 const getSchedules = computed(() => {
-  return schedulesStore.getSchedules;
+    return schedulesStore.getSchedules;
+})
+
+const getDate = computed(() => {
+    return dateStore.getEventsTable;
+})
+
+const getPeriod = computed(() => {
+    return periodStore.getPeriodCurrent;
 })
 
 onMounted(() => {
-  schedulesStore.searchSchedules();
+    schedulesStore.searchSchedules();
+    dateStore.searchEventsTable(['Entregables', 'fecha de corte']);
+    periodStore.searchPeriodsCurrent();
+    setTimeout(() => {
+        console.log(getPeriod.value[0]['nombre_periodo'])
+    }, 3000);
 })
 
 let scheduleID = ref('');
 let detachSchedule = ref('')
 
 function idListener(dato) {
-  if (dato) {
-    console.log(dato)
-    scheduleID.value = dato
-    let temp = schedulesStore.options.schedules.filter((schedule) => scheduleID.value == schedule.id_horario);
-    detachSchedule.value = temp[0]
-    //console.log(detachSchedule.value)
-  }
+    if (dato) {
+        console.log(dato)
+        scheduleID.value = dato
+        let temp = schedulesStore.options.schedules.filter((schedule) => scheduleID.value == schedule.id_horario);
+        detachSchedule.value = temp[0]
+        //console.log(detachSchedule.value)
+    }
 
 }
 
+
+const periodNow = ref('')
+
+watch(periodStore.$state, (newState) => {
+    console.log('observando', newState.options.currentPeriod[0])
+    if (newState.options.currentPeriod && newState.options.currentPeriod[0]) {
+        periodNow.value = getPeriod.value[0]['nombre_periodo'];
+    }
+});
 </script>
 
 <template>
+    <!--
     <div class="topDiv">
         <div>
             <h1>Fechas Académicas</h1>
@@ -46,18 +74,30 @@ function idListener(dato) {
             <Entity_Button message="Periodos" :onClick="changeState" />
         </div>
     </div>
+    -->
+    
 
-  <DashTable @takenID="idListener" :tableHead="['Dia de la Semana', 'Hora de Entrada', 'Hora de Salida']"
-    :forBody="['dia_semana', 'hora_inicio', 'hora_final']" :options="true" :forTable="getSchedules"
-    mainButton="Añadir horario" h1Title="Gestión de horarios" h3Title="Aquí puedes crear y gestionar horarios"
-    :toChangeState="buttonChange" :lessOptions="true" :assignedOptionsSchedules="true" :typeGestion="'schedule'">
-  </DashTable>
+    <DashTable 
+        @takenID="idListener" 
+        :tableHead="['Nombre', 'Tipo', 'Semana', 'Fecha']"
+        :forBody="['nombre_largo', 'tipo_fecha', 'idSemana', 'fecha_especial']" 
+        :options="true" :forTable="getDate"
+            
+        mainButton="Añadir Fecha" 
+        h1Title="Fechas Académicas" 
+        :h3Title="`Periodo actual: ${periodNow}`"
 
-  <CreateSchedule :toChangeState="buttonChange" :state="buttonState.bState" />
-  <Modal_assigned_schedules :toChangeState="buttonChange" :state="buttonState.M2State" />
-  <EditSchedule v-if="detachSchedule" :scheduleDetail="detachSchedule" :toChangeState="buttonChange"
-    :state="buttonState.eState" />
-  <!--   <EditSchedule :toChangeState="buttonChange" :state="buttonState.eState" /> -->
+        :toChangeState="buttonChange" 
+        :lessOptions="true" 
+        :assignedOptionsSchedules="true" 
+        :typeGestion="'schedule'">
+    </DashTable>
+
+    <CreateSchedule :toChangeState="buttonChange" :state="buttonState.bState" />
+    <Modal_assigned_schedules :toChangeState="buttonChange" :state="buttonState.M2State" />
+    <EditSchedule v-if="detachSchedule" :scheduleDetail="detachSchedule" :toChangeState="buttonChange"
+        :state="buttonState.eState" />
+    <!--   <EditSchedule :toChangeState="buttonChange" :state="buttonState.eState" /> -->
 
 </template>
 
