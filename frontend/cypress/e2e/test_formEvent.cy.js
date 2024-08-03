914fcdc38d913c5527d9c2457af7906e58eb3ca7
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-08-03 19:36:14 -0400

    GDP-37 test formEvents cerrar formularios

--- frontend/cypress/e2e/test_formEvent.cy.js
+++ frontend/cypress/e2e/test_formEvent.cy.js
@@ -1,3 +1,5 @@
+let correo = "roberto@gmail.com";
+let clave = "profe12345";
 function realizarLogin(correo, clave) {
   cy.request({
     method: 'POST',
@@ -12,26 +14,43 @@ function realizarLogin(correo, clave) {
       expect(response.status).to.eq(200);
     });
 }
+const changeNav = (action = '') => {
+  cy.get('span.' + action).click();
+}
+
 
 
 describe('test formulario', () => {
-  beforeEach(() => {
-    cy.visit('/admin-dsh/planificaciones')
+  before(() => {
+    // Realiza el login una vez antes de todas las pruebas
+    cy.visit('/login');
     //Rellenar con datos registrados
-    cy.get('#correo').type("roberto@gmail.com")
-    cy.get('#clave').type("profe12345")
+    cy.get('#correo').type(correo)
+    cy.get('#clave').type(clave)
     cy.get('.form-submit').click() //Enviar
+    //si tiene éxito llega al calendario
+    cy.url().should('eq', 'http://localhost:3000/calendario')
+    changeNav('collapse-icn'); //Cerrar nav
+    //guardar las cookies después del login
+    cy.getCookies().then((cookies) => {
+      cy.writeFile('cookies.json', cookies);
+    });
+  });
 
-    realizarLogin("roberto@gmail.com", "profe12345") //Hacer una petición post con esos datos
 
-    cy.url().should('eq', 'http://localhost:3000/calendario') //si tiene éxito llega al calendario
+  beforeEach(() => {
+    cy.readFile('cookies.json').then((cookies) => {
+      cookies.forEach((cookie) => {
+        cy.setCookie(cookie.name, cookie.value);
+      });
+    });
 
     //Pasa el login y se dirige al calendario (donde lo redirige la función por defecto)
-
     cy.visit('/calendario')
     cy.contains('a', 'Planificaciones').click()
         
     cy.url().should('eq', 'http://localhost:3000/admin-dsh/planificaciones')
+    changeNav('collapse-icn'); //Cerrar nav
     cy.contains('h1', 'Periodo Académico 2024') //comprobando que la página tenga esto
     cy.contains('b', 'Trimestre:') //comprobando que la página tenga esto
     cy.contains('b', 'Profesor:') //comprobando que la página tenga esto
@@ -44,6 +63,7 @@ describe('test formulario', () => {
   it('Validacion del formulario', () => {
     //Escoger y llenar formulario
     cy.get('#0-3-events').click()
+    cy.wait(2000)
     cy.get('#0-3-events').contains('button', 'Crear evento').click()
     cy.get('.formCreateEvent_select').select('Conferencia') 
     cy.get('input[placeholder="Nombre corto"]').type("Defensa de la actividad").should('have.attr', 'type', 'text')
@@ -59,8 +79,14 @@ describe('test formulario', () => {
     cy.get('#timeActivity').type("03:00").should('have.attr', 'type', 'time');
     cy.get('.submitB').click() 
     cy.contains('h4', 'Registro exitoso') //comprobando que la página tenga esto
+    cy.wait(1700)
     cy.get('.modalMessage').contains('button', 'cerrar X').click()
+    cy.wait(500)
+    cy.get('#modalE-0-3-CE').click()
+    cy.wait(2500)
+    cy.get('#modalE-0-3').click()
   })
+  
   it('Validacion del formulario con un dato incorrecto', () => {
     //Escoger y llenar formulario
     cy.get('#0-3-events').click()
@@ -79,5 +105,5 @@ describe('test formulario', () => {
     cy.get('#timeActivity').type("hola mundo").should('have.attr', 'type', 'time');
     cy.get('.submitB').click() 
     cy.contains('h4', 'Registro exitoso') //comprobando que la página tenga esto
-  }) 
+  })
 })
