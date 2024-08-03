bbca33878bf8fec1294859a13a26081c58aaffee
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-08-03 18:09:17 -0400

    GDP-37 vista de test

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -4,10 +4,49 @@
 </script>
 
 <template>
-    <!-- <DashHeader/>
-    <DashSidebar/>  -->
+    <div class="topDivTest">
+        <div class="titulosTest">
+            <h1>Vista de Test</h1>
+            <p>Aquí puede ir cualquier cosa</p>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
+@import "../assets/scss/variables.scss";
 
+h1 {
+    color: $color7;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.58);
+    font-family: Ubuntu;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+p {
+    color: $color7;
+    text-shadow: 0px 1px 4px rgba(0, 0, 0, 0.74);
+    font-family: Ubuntu;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.topDivTest {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    margin: 3%;
+    padding-bottom: 50vh;
+}
+
+
+.titulosTest {
+    margin-left: 22vw;
+}
 </style>
