6159950ae2d336c283bb37d248b3fe78f3b73ab4
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-08-03 17:28:17 -0400

    GDP-37 test nav realizar login

--- frontend/cypress/e2e/test_nav.cy.js
+++ frontend/cypress/e2e/test_nav.cy.js
@@ -2,14 +2,51 @@ let correo = "direccion_ingenieria@gmail.com";
 let clave = "root12345";
 
 describe('test calendario', () => {
-  it('Verificar el funcionamiento del Nav', () => {
-    cy.visit('/login')
+  before(() => {
+    // Realiza el login una vez antes de todas las pruebas
+    cy.visit('/login');
     //Rellenar con datos registrados
     cy.get('#correo').type(correo)
     cy.get('#clave').type(clave)
     cy.get('.form-submit').click() //Enviar
-    cy.url().should('eq', 'http://localhost:3000/calendario') //si tiene éxito llega al calendario
+    //si tiene éxito llega al calendario
+    cy.url().should('eq', 'http://localhost:3000/calendario')
+    cy.get('span.collapse-icn').click();
+    //guardar las cookies después del login
+    cy.getCookies().then((cookies) => {
+      cy.writeFile('cookies.json', cookies);
+    });
+  });
 
+  beforeEach(() => {
+    cy.readFile('cookies.json').then((cookies) => {
+      cookies.forEach((cookie) => {
+        cy.setCookie(cookie.name, cookie.value);
+      });
+    });
+    // Visita la página principal (vacía) antes de cada prueba
+    cy.visit('/admin-dsh/');
+  });
+
+  it('Comprobar que el nav abre y cierra', () => {
+    //Revisar si está abierto
+    cy.get('span.header-side')
+      .find('div.img-container')
+      .should('exist');
+
+    cy.wait(1000)
+
+    //Cerrar y verificar
+    cy.get('span.collapse-icn').click();
+    cy.get('span.header-side')
+      .find('div.img-container')
+      .should('not.exist');
+
+    cy.wait(2000)
+  })
+
+
+  it('Comprobar que el nav redirige a Secciones', () => {
     //Revisar si está abierto
     cy.get('span.header-side')
       .find('div.img-container')
@@ -17,6 +54,7 @@ describe('test calendario', () => {
 
     cy.get('a[href*="/admin-dsh/secciones"]').click() //Hacer click en nav
     cy.url().should('eq', 'http://localhost:3000/admin-dsh/secciones') //si tiene éxito llega a la página
+    cy.get('span.collapse-icn').click();
   })
-  
+
 }) 
